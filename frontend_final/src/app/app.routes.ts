ad0fd2949ad26ed52c30ed4210e3b4d0698cbe03
savaged-22 <juanpabloespinosa2014@gmail.com>
2026-04-28 14:01:38 +0000

    add: changes for landing pages

--- frontend_final/src/app/app.routes.ts
+++ frontend_final/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './pages/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPage }
+];
---
+++ frontend_final/src/app/pages/landing/landing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  templateUrl: './landing.html',
+  styleUrl: './landing.css'
+})
+export class LandingPage {
+  // Logic for landing page can go here
+}
